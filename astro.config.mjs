2127b718985d189fc9d2f8b1464b17c21d9bd3b7
Richard Pisarski <rspisarski@gmail.com>
2026-01-26 00:36:33 -0500

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://goopfinder.com',
+  integrations: [tailwind(), sitemap()],
 });
